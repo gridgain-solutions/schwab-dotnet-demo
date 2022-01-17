1e34d792a04d93de416a3f3952360f983f53f3ef
dggneubert <90294353+dggneubert@users.noreply.github.com>
2022-01-17 12:53:15 -0800

    Export files referred to in build scripts

---
+++ build/schwab-demo-export-dotnet.sh
@@ -0,0 +1,5 @@
+# Export Schwab Demo environment variables
+
+export SCHWAB_DEMO_GG_CONFIGS_PATH='gg'
+export SCHWAB_DEMO_GG_JVMCLASSPATH=
+# export SCHWAB_DEMO_GG_JVMCLASSPATH='gg/userlibs/model-1.0.jar;gg/userlibs/application-1.0.jar'
---
+++ build/schwab-demo-export-java.sh
@@ -0,0 +1,6 @@
+# Export Schwab Demo environment variables
+
+export PATH=$PATH:$IGNITE_HOME/bin
+
+export SCHWAB_DEMO_GG_CONFIGS_PATH=gg
+export SCHWAB_DEMO_GG_JVMCLASSPATH=
