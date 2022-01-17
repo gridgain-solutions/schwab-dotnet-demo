71cbdff7e39a3ed6d5822f8aa47d255cfa269e00
dggneubert <90294353+dggneubert@users.noreply.github.com>
2022-01-17 12:50:22 -0800

    Dockerfiles and build scrips for Java GG client...
    and Java GG Servers (although GG Java servers are not used in the Schwab demo)

---
+++ build/build-schwab-java-base-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set paths to "Schwab demo" configuration folders and files
+SCHWAB_DEMO_HOME=/mnt/c/clients/Schwab/demo
+SCHWAB_DEMO_DOCKERFILE=${SCHWAB_DEMO_HOME}/build/Dockerfile.java-base
+SCHWAB_DEMO_JAVA_BUILD_CONTEXT_FOLDER=${SCHWAB_DEMO_HOME}/ggnode
+
+# Set build context to "demo" java project folder and export env variables.
+source ${SCHWAB_DEMO_HOME}/build/schwab-demo-export-java.sh
+
+# Postion current directory to the IntelliJ project folder to use as the context folder to build the docker image.
+cd ${SCHWAB_DEMO_JAVA_BUILD_CONTEXT_FOLDER}
+
+# Display build context and env variables (for debugging/verification purposes)
+echo -e "\nUsing docker file: ${SCHWAB_DEMO_DOCKERFILE}"
+echo -e "Build docker image: schwab-java-server in folder: ${PWD}"
+echo "With environment variable settings:"
+echo "     SCHWAB_DEMO_GG_CONFIGS_PATH: ${SCHWAB_DEMO_GG_CONFIGS_PATH}"
+echo -e "     SCHWAB_DEMO_GG_JVMCLASSPATH: ${SCHWAB_DEMO_GG_JVMCLASSPATH}\n"
+
+# Build the docker image: schwab-java-base:latest
+docker build -f ${SCHWAB_DEMO_DOCKERFILE} -t sdemo-java-base:latest .
+# docker build -f ${SCHWAB_DEMO_DOCKERFILE} --progress plain --no-cache -t sdemo-java-base:latest .
+
+docker tag sdemo-java-base:latest dgneubert/sdemo-java-base:latest
+docker push dgneubert/sdemo-java-base:latest
+
+# Run the docker image to test it.
+echo -e "\nRun local docker image: sdemo-java-base\n"
+docker run -it --rm --network host sdemo-java-base
+# docker run -it -e CONFIG_URI=/app/config/server-config.xml --rm --network host sdemo-java-base
---
+++ build/build-schwab-java-server-image.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set paths to "Schwab demo" configuration folders and files
+SCHWAB_DEMO_HOME=/mnt/c/clients/Schwab/demo
+SCHWAB_DEMO_DOCKERFILE=${SCHWAB_DEMO_HOME}/build/Dockerfile.java-server
+SCHWAB_DEMO_JAVA_BUILD_CONTEXT_FOLDER=${SCHWAB_DEMO_HOME}/ggnode
+
+# Set build context to "demo" java project folder and export env variables.
+source ${SCHWAB_DEMO_HOME}/build/schwab-demo-export-java.sh
+
+# Postion current directory to the IntelliJ project folder to use as the context folder to build the docker image.
+cd ${SCHWAB_DEMO_JAVA_BUILD_CONTEXT_FOLDER}
+
+# Display build context and env variables (for debugging/verification purposes)
+echo -e "\nUsing docker file: ${SCHWAB_DEMO_DOCKERFILE}"
+echo -e "Build docker image: schwab-java-server in folder: ${PWD}"
+echo "With environment variable settings:"
+echo "     SCHWAB_DEMO_GG_CONFIGS_PATH: ${SCHWAB_DEMO_GG_CONFIGS_PATH}"
+echo -e "     SCHWAB_DEMO_GG_JVMCLASSPATH: ${SCHWAB_DEMO_GG_JVMCLASSPATH}\n"
+
+# Build the docker image: schwab-java-server:latest
+docker build -f ${SCHWAB_DEMO_DOCKERFILE} -t sdemo-java-server:latest .
+# docker build -f ${SCHWAB_DEMO_DOCKERFILE} --progress plain --no-cache -t sdemo-java-server:latest .
+
+docker tag sdemo-java-server:latest dgneubert/sdemo-java-server:latest
+docker push dgneubert/sdemo-java-server:latest
+
+# Run the docker image to test it.
+echo -e "\nRun local docker image: sdemo-java-server\n"
+docker run -it -e CONFIG_URI=/app/config/server-config.xml --rm --network host sdemo-java-server
